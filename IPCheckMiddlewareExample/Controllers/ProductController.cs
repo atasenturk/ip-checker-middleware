70b7de333daadfc2f4f4eba71cba315f66859c9d
ata şentürk <atasentrk4526@gmail.com>
2024-01-22 10:39:05 +0300

    Add project files.

---
+++ IPCheckMiddlewareExample/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IPCheckMiddlewareExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new Product[]
+            {
+                new Product { Id = 1, Name = "Product 1", Description = "Description 1" },
+                new Product { Id = 2, Name = "Product 2", Description = "Description 2" },
+                new Product { Id = 3, Name = "Product 3", Description = "Description 3" },
+                new Product { Id = 4, Name = "Product 4", Description = "Description 4" },
+                new Product { Id = 5, Name = "Product 5", Description = "Description 5" },
+            });
+        }
+    }
+}
---
+++ IPCheckMiddlewareExample/IPCheckMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+
+namespace IPCheckMiddlewareExample
+{
+    public class IPCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly List<IPAddress> _allowedIPs;
+
+        public IPCheckMiddleware(RequestDelegate next)
+        {
+            _next = next;
+            _allowedIPs = new List<IPAddress>
+            {
+                // Allowed IP addresses
+                IPAddress.Parse("127.0.0.1"),
+                IPAddress.Parse("::1")
+                // Not allowed IP adressess
+                //IPAddress.Parse("127.0.0.1"),
+                //IPAddress.Parse("::2")
+            };
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/api/swagger"))
+            {
+                await _next.Invoke(context);
+                return;
+            }
+            var clientIp = context.Connection.RemoteIpAddress;
+            if (clientIp is null)
+            {
+                await context.Response.WriteAsync("Client IP address not available.");
+            }
+
+            if (!_allowedIPs.Contains(clientIp!))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("You are not allowed to access this resource.");
+                return;
+            }
+            await _next.Invoke(context);
+        }
+    }
+}
